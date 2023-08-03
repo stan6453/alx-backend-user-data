7ddf59457e875177e5dd3e61d873baabf12fef78
stanley <stanley6453@gmail.com>
2023-08-03 10:18:15 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,28 @@
 """TODO : Desc"""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """init function"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """"""
+        log_message = super().format(record)
+        for field in self.fields:
+            log_message = filter_datum(
+                [field], self.REDACTION, log_message, self.SEPARATOR)
+        return log_message
 
 
 def filter_datum(fields: List[str],
