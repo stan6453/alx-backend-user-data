6946da8352bfa629e972d27b98a46789c1d1e312
stanley <stanley6453@gmail.com>
2023-08-07 21:57:41 +0100

    Improve def require_auth(self, path, excluded_paths) by allowing * at the end of excluded paths.
    Example for excluded_paths = [/api/v1/stat*]:
    
        /api/v1/users will return True
        /api/v1/status will return False
        /api/v1/stats will return False

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,16 @@ from flask import request
 from typing import TypeVar, List
 
 
+def verify_partial_path(path: str, excluded_paths: List[str]) -> bool:
+    """returns true if partial path requires authentication"""
+    if len(excluded_paths) == 0:
+        return True
+    for item in excluded_paths:
+        if path.startswith(item[:-1]):
+            return False
+    return True
+
+
 class Auth():
     """AUTHORIZATION Class"""
 
@@ -12,9 +22,16 @@ class Auth():
         """returns true if path requires authentication"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
+
         if path[-1] != '/':
             path += '/'
-        if path not in excluded_paths:
+
+        full_path = [path1 for path1 in excluded_paths if not path1.find('*')]
+        # path like : ["/api/v1/stat*"]
+        partial_path = [
+            path1 for path1 in excluded_paths if path1.endswith('*')]
+
+        if path not in full_path or verify_partial_path(path, partial_path):
             return True
         return False
 
