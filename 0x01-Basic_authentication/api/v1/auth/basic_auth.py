fdd938d0ac86cb3c8c84dbca6317695751ff9952
stanley <stanley6453@gmail.com>
2023-08-07 19:54:56 +0100

    implemented BasicAuth.decode_base64_authorization_header to decode base64 strings

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic auth"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
         if arr[0] != 'Basic':
             return None
         return arr[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """decode base64 encoded string and return the string"""
+        if base64_authorization_header is None or\
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            base64_str = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return base64_str
+        except Exception:
+            return None
