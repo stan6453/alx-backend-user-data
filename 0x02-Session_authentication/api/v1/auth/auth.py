55ddebf3d5681e17b1947ebc6a7fc4a0b22f9280
stanley <stanley6453@gmail.com>
2023-08-10 20:12:34 +0100

    initialized a SessionAuth class for managing user sessions for the software

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""AUTHORIZATION Class"""
+
+from flask import request
+from os import getenv
+from typing import TypeVar, List
+
+
+def require_auth_for_dynamic_path(path: str,
+                                  excluded_paths: List[str]) -> bool:
+    """returns true if partial path requires authentication"""
+    if len(excluded_paths) == 0:
+        return True
+    for item in excluded_paths:
+        if path[:-1].startswith(item[:-1]):
+            return False
+    return True
+
+
+class Auth():
+    """AUTHORIZATION Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns true if path requires authentication"""
+        verify_dynamic_path = False
+        verify_fixed_path = False
+
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        fixed_path = [
+            path1 for path1 in excluded_paths if not path1.endswith('*')]
+        # path like : ["/api/v1/stat*"]
+        dynamic_path = [
+            path1 for path1 in excluded_paths if path1.endswith('*')]
+
+        if path not in fixed_path:
+            verify_fixed_path = True
+        if require_auth_for_dynamic_path(path, dynamic_path):
+            verify_dynamic_path = True
+
+        return verify_fixed_path and verify_dynamic_path
+
+    def authorization_header(self, request=None) -> str:
+        """get auth info from the authorization header"""
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """get and return the current user"""
+        return None
+
+    def session_cookie(self, request=None):
+        """returns the session cookie value (i.e the session ID) from a request"""
+        if request is None:
+            return None
+        session_cookie_name = getenv('SESSION_NAME')
+        return request.cookies.get(session_cookie_name)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Session authentication
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+import uuid
+
+
+class SessionAuth(Auth):
+    """basic auth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create a unique session for a user
+
+        Note:The same user_id can have multiple Session ID - indeed,
+        the user_id is the value in the dictionary user_id_by_session_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None or type(session_id) is not str:
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns a User instance based on a cookie value"""
+        from models.user import User
+
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
