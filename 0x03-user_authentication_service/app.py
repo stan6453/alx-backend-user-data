fccfdca35738b1bf955b8be6f2fcefb5cadfd546
stanley <stanley6453@gmail.com>
2023-08-17 15:55:48 +0100

    implemented /sessions route to login a user and create a session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic flask app"""
 
 from auth import Auth
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 
 AUTH = Auth()
 
@@ -26,6 +26,19 @@ def register_user():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """login endpoint"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
