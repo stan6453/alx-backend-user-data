b7da217485a5fb2ce995999065e91348cb9509b7
stanley <stanley6453@gmail.com>
2023-08-17 16:59:22 +0100

    implemented Auth.get_reset_password_token that stores and returns a new reset_token for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,13 @@ class Auth:
         except NoResultFound:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str):
+        """store and return a new reset_token"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
