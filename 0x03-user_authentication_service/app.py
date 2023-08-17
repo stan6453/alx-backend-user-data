78dbcf657ac9b6b8213fa3de9d12b435ad9b8bb8
stanley <stanley6453@gmail.com>
2023-08-17 16:20:23 +0100

    implemented /sessions DELETE route that deletes the user session from storage and logges them out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Basic flask app"""
 
 from auth import Auth
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, redirect, url_for
 
 AUTH = Auth()
 
@@ -39,6 +39,17 @@ def login():
         response.set_cookie('session_id', session_id)
         return response
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """logout endpoint"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect(url_for('home'))
+    abort(403)
 
 
 if __name__ == "__main__":
