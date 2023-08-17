52db6193dbbcf390e0d657df2697d73f2e686967
stanley <stanley6453@gmail.com>
2023-08-17 17:07:45 +0100

    implemented /reset_password POST route for requesting a password reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,16 @@ def profile() -> str:
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> str:
+    """responds with a password reset token"""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
