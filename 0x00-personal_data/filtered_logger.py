02f649d8eb1ce705145125bee6d59ae2ae779a83
stanley <stanley6453@gmail.com>
2023-09-19 05:59:27 +0100

    created get_logger function that returns a logger with the custom RedactingFormatter as formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,7 +16,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: Sequence[str]):
+    def __init__(self, fields: List[str]):
         """init function"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
