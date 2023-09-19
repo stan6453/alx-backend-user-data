b04047db0dd7a8c58b6872020710a0cb8e229f6d
stanley <stanley6453@gmail.com>
2023-09-19 19:12:40 +0100

    created is_valid function in encrypt_password.py for validating encrypted password match

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ def hash_password(password: str) -> bytes:
     """return hashed password"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """Use bcrypt to validate that the provided
+    password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
