2c0e491babb7fbc5ee07a71ee93e57ff2ba4267e
stanley <stanley6453@gmail.com>
2023-09-19 07:49:57 +0100

    created get_db function that returns a mysql.connector.connection.MySQLConnection instance

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -77,5 +77,5 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=getenv('PERSONAL_DATA_DB_NAME', 'holberton')
+        database=getenv('PERSONAL_DATA_DB_NAME',)
     )
